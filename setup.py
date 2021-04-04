7ef9c47dc1f1369751ea56200d6597f07226d5cf
Miguel Grinberg <miguel.grinberg@gmail.com>
2021-04-04 13:18:44 +0100

    Initial attempt to support stream updates over WebSocket

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ setup(
     include_package_data=True,
     platforms='any',
     install_requires=[
-        'Flask'
+        'Flask',
+        'Flask-Sock',
     ],
     classifiers=[
         'Environment :: Web Environment',
