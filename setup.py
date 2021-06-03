a9c1375e444e8ef9360b1ded03977391a4e3aa7c
Miguel Grinberg <miguel.grinberg@gmail.com>
2021-06-03 12:51:00 +0100

    improved project structure

--- setup.py
+++ setup.py
@@ -1,37 +1,3 @@
-"""
-Turbo-Flask
------------
+import setuptools
 
-Use Hotwire Turbo in your Flask application.
-"""
-from setuptools import setup
-
-
-setup(
-    name='Turbo-Flask',
-    version='0.5.0',
-    url='http://github.com/miguelgrinberg/turbo-flask/',
-    license='MIT',
-    author='Miguel Grinberg',
-    author_email='miguel.grinberg@gmail.com',
-    description='Use Hotwire Turbo in your Flask application',
-    long_description=__doc__,
-    packages=['turbo_flask'],
-    zip_safe=False,
-    include_package_data=True,
-    platforms='any',
-    install_requires=[
-        'Flask',
-        'Flask-Sock',
-    ],
-    classifiers=[
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
-)
+setuptools.setup()
